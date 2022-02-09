fde8aed07d0c9e69b9628f1523b4c4784088efff
Cristian <492508@student.saxion.nl>
2022-02-09 15:15:04 +0100

    Main menu UI

---
+++ party game/Assets/Script/MainMenuNav.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNav : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+}
