a3a924057590b6fc0f7cc55157d2eed816ba66e0
Simona <simonab518@gmail.com>
2022-02-16 21:30:11 +0100

    Cat Animations

---
+++ party game/Assets/Script/Animations.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class Animations : MonoBehaviour
+{
+    
+    private Animator animator;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject PlayerScoreGetter = GameObject.Find("Game Manager");
+        ScoreManager CatScore = PlayerScoreGetter.GetComponent<ScoreManager>();
+        
+        if (CatScore.P1Score >= 3 && CatScore.P1Score<6)
+        {
+            animator.Play("Cat_Idle_Tipsy");
+        }
+        if (CatScore.P1Score >= 6)
+        {
+            animator.Play("Cat_Idle_Drunk");
+        }
+    }
+}
